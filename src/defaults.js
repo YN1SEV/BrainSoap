543e14c705db3dbe95e3780c636f8b6a7bd73c2f
YN1SEV <t21.jakob@gmail.com>
2026-05-11 17:02:56 +0200

    url tracking and sanitation

--- src/defaults.js
+++ src/defaults.js
@@ -3,7 +3,7 @@ const defaultSettings = {
   exist: true, // flag to check if user deleted browser data
   theme: "light", // make this browser default
   notificationsEnabled: true, // note to self: change to false
-  limit: 1800, // TODO: make this per domain
+  limit: 5, // TODO: make this per domain
   // save all the dommscrolling sites
-  urls: ["http://youtube.com/shorts", "http://instagram.com/reels", "http://tiktok.com/"]
+  urls: ["https://youtube.com/shorts", "https://instagram.com/reels", "https://tiktok.com/"]
 };
