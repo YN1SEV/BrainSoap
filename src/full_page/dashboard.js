6482270e98a7f830599ead216f757e066f4a1850
moritz2755 <moritz.weingessl3@gmail.com>
2026-05-17 14:34:05 +0200

    improved section navigation

--- src/full_page/dashboard.js
+++ src/full_page/dashboard.js
@@ -1,19 +1,34 @@
 
 
-const navItems = document.querySelectorAll('.navItem');
-const contentSections = document.querySelectorAll('.content');
+const navItemsLeftBar = document.querySelectorAll('.navItem');
+const navItemsContent = document.querySelectorAll('.content');
 
-navItems.forEach(item => {
-    item.addEventListener('click', () => {
-        const section = item.dataset.section;
+function getSectionFromHash() { return location.hash.replace('#/', '') || 'home'; }
+function setHash(section)     { location.hash = `#/${section}`;}
 
-        navItems.forEach(n => n.classList.remove('active'));
-        item.classList.add('active');
+function updateDisplay(section) {
+    
+    const activeNav = document.querySelector(`[data-section="${section}"]`);
+    const activeContent = document.getElementById(`content${section[0].toUpperCase() + section.slice(1)}`);
+    // ^ This gets element by id: contentHome, contentRules, contentStats etc.
 
-        contentSections.forEach(i => i.classList.remove('active'));
 
-        document
-            .getElementById(`content${section[0].toUpperCase() + section.slice(1)}`)
-            ?.classList.add('active');
-    });
+    // Chooses what displayed (set active)
+    navItemsLeftBar.forEach(n => n.classList.remove('active'));
+    navItemsContent.forEach(n => n.classList.remove('active'));
+
+    activeNav    ?.classList.add('active');
+    activeContent?.classList.add('active');
+}
+
+navItemsLeftBar.forEach(item => {item.addEventListener('click', () => {
+    const section = item.dataset.section;
+    setHash(section);
+    updateDisplay(section);
+})});
+
+window.addEventListener('hashchange', () => {
+    updateDisplay(getSectionFromHash());
 });
+
+updateDisplay(getSectionFromHash());
