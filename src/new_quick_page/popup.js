f35beec7b9d6205f0794f1b54c4c9646b2638703
moritz2755 <moritz.weingessl3@gmail.com>
2026-05-21 21:14:26 +0200

    resume working on different device

--- src/new_quick_page/popup.js
+++
@@ -1,6 +0,0 @@
-function updateStats(streak, minutes) {
-    document.getElementById("display-streak").textContent = `${streak} Streak`;
-    document.getElementById("display-time").textContent = `${minutes} min left`;
-  
-}
-updateStats(7, 12);
