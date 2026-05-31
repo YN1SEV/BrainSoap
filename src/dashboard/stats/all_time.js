3827d58304563a755f956faafcf95fdb58355861
moritz2755 <moritz.weingessl3@gmail.com>
2026-05-31 17:56:17 +0200

    cleanup 2

---
+++ src/dashboard/stats/all_time.js
@@ -0,0 +1,9 @@
+document.getElementById("focus-hours").textContent       = `Temp`;
+document.getElementById("focus-sessions").textContent    = `Temp`;
+document.getElementById("avg-focus-session").textContent = `Temp`;
+document.getElementById("scroll-hours").textContent      = `Temp`;
+document.getElementById("scroll-attempts").textContent   = `Temp`;
+document.getElementById("scrolls-blocked").textContent   = `Temp`;
+document.getElementById("best-streak").textContent       = `Temp`;
+document.getElementById("sites-blocked").textContent     = `Temp`;
+document.getElementById("active-days").textContent       = `Temp`;
