d86401eae2aeb08f2453dbf5c4b725fc9739bf19
moritz2755 <moritz.weingessl3@gmail.com>
2026-06-01 12:36:03 +0200

    Cleanup and Aria Labels

--- src/quick_page/popup.js
+++ src/quick_page/popup.js
@@ -1,6 +1,12 @@
-function updateStats(streak, minutes) {
-    document.getElementById("display-streak").textContent = `${streak} Streak`;
-    document.getElementById("display-time").textContent = `${minutes} min left`;
-  
+const dStreak  = document.getElementById("display-streak");
+const dTime    = document.getElementById("display-time");
+const dTimeBar = document.getElementById("time-bar");
+
+
+function updateStats(streak, minutes, percentage) {
+    dStreak.textContent = `${streak} Streak`;
+    dTime.textContent = `${minutes} min left`;
+    dTimeBar.style.setProperty("--time-bar-progress", `${percentage*100}%`);
 }
-updateStats(7, 12);
+
+updateStats(7, 12, 0.5);
