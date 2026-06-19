e4864093fae2ef5fdc91ab23b6381a1c1a4a770c
YN1SEV <t21.jakob@gmail.com>
2026-06-19 14:49:52 +0200

    broken

--- src/utils/defaults.js
+++ src/utils/defaults.js
@@ -6,12 +6,18 @@ const defaultSettings = {
   
   // available actions: "notify" = message, "popup" = popup window, "redirect":url = redirect to a different page
   timers: {"default": {"limit": 5, "actions": ["notify", "popup", "redirect"], "redirectUrl": "https://www.linkedin.com/jobs/"},
-            "example1": {"limit": 5, "actions": ["image"], "imagePath": "assets/visuals/stop.png"},
+          "example1": {"limit": 5, "actions": ["image"], "imagePath": "assets/visuals/stop.png"},
             },
 
   timerMap: {"youtube.com/shorts": ["default"],
-              "instagram.com/reels": ["example1"],
-              "tiktok.com": ["default"],
-              "engage.cloud.microsoft/main":["default"]
+             "instagram.com/reels": ["example1"],
+             "tiktok.com": ["default"],
+             "engage.cloud.microsoft/main":["default"]
+  },
+
+  groups:{
+    "socials":["instagram.com/reels"],
+    "shopping":[],
+    "other":[]
   }
 };
